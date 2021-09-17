80b159d6906decbdbee8eae99064d5e9128d1f00
Vincent Chooi <vinceecws@gmail.com>
2021-09-17 03:48:57 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import datetime
-from constants import *
+from constants import UNITS_LIB
 
+# Application configs
 LOCAL_TIMEZONE = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
 API_KEY = "YOUR OPENWEATHER API KEY"
 UNITS = "imperial"
@@ -21,4 +22,5 @@ PLOT_MARKER_2 = "heart"
 # Config parameters checking
 assert UNITS in UNITS_LIB, f"Invalid unit system. Must be one of {list(UNITS_LIB.keys())}"
 assert PLOT_SIZE_X > 0 and PLOT_SIZE_Y > 0, \
-    f"Invalid plot sizes of PLOT_SIZE_X (width)={PLOT_SIZE_X}, PLOT_SIZE_Y (height)={PLOT_SIZE_Y}. Both must be > 0."
+    f"Invalid plot sizes of PLOT_SIZE_X (width)={PLOT_SIZE_X}, " \
+    f"PLOT_SIZE_Y (height)={PLOT_SIZE_Y}. Both must be > 0."
